22674cf0fd1f82e5a3c9fd8deec8a665c026e88e
Argentfire <Silv3rfire@hotmail.com>
2023-05-31 13:59:17 +0300

    Replaced vanilla element integration with AngularJS

--- js/JourneyController.js
+++ js/JourneyController.js
@@ -1,7 +1,7 @@
 (function() {
     var module = angular.module("Agency");
 
-    var JourneyController = function($scope, $http, $injector) {
+    var JourneyController = function($scope, $http) {
         
         var journeys = [];
         var onJourneysComplete = function(response) {
@@ -16,28 +16,73 @@
                 }
             }
             $scope.destinations = destinations;
-            AddFilterDestBtnsClickEvent();
+            document.getElementById("btnAll").style.backgroundColor = "rgba(82, 124, 139,1)";
+            document.getElementById("btnDestAll").style.backgroundColor = "rgba(82, 124, 139,1)";
+            // AddFilterDestBtnsClickEvent();
         }
 
-        $scope.testFunc = function()
+        $scope.asdf = function(event) {
+            var elementId = event.target.id;
+            alert("Element ID: " + elementId);
+        }
+
+        function GetParentElement(elem) {
+            var triggerType = elem.attr('id');
+            var parent = document.getElementById(triggerType).parentElement;
+
+            return parent;
+        }
+
+        $scope.testFunc = function(e)
         {
             var elem = angular.element(e.srcElement);
             var triggerType = elem.attr('id');
 
-            var veh = triggerType.replace("btn", "");
+            for(var foo of document.getElementsByClassName("vehicle-filter-button")) {
+                foo.style.backgroundColor = "rgba(121, 165, 181, 1)";
+            }
 
-            let filteredJourneys = [];
-            if(veh.includes("All")) {
-                DisplayJourneys(journeys);
-                return;
-            } 
+            elem.css('background', 'rgba(82, 124, 139,1)');
+            
+            var veh = triggerType.replace("btn", "");
+            var journeyElements = document.getElementsByClassName("journeyData");
+            var destinationFilters = document.getElementsByClassName("destination-filter-button");
+            for(var item of journeyElements) {
+                item.style.display = "none";
+            }
+            for(var item of destinationFilters) {
+                if(item.value.includes("All") == false)
+                    item.style.display = "none";
+            }
+            var filteredJourneys = [];
+            if(veh.includes("All")){
+                for(var item of journeyElements) {
+                    item.style.display = "flex";
+                }
+                for(var item of destinationFilters) {
+                    item.style.display = "block";
+                }
+
+            }
             else {
-                for(var i of journeys) {
-                    if(i.vehicleModel.includes(veh))
-                        filteredJourneys.push(i);
+                for(var item of journeyElements) {
+                    if(item.textContent.includes(veh)) {
+                        item.style.display = "flex";
+                        filteredJourneys.push(item);
+                    }
+                }
+                for(var item of destinationFilters) {
+                    for(var i = 0; i < filteredJourneys.length; i++)
+                    {
+                        if(filteredJourneys[i].innerHTML.includes(item.value)) {
+                            item.style.display = "block";
+                        }
+                    }
                 }
             }
         }
+
+
 
         var filterDest = function(e) {
             var elem = angular.element(e.srcElement);
@@ -54,6 +99,13 @@
             }
         }
 
+        $scope.QueryVehicleType = function(vehicleModel)
+        {
+            var veh = vehicleModel;
+            veh = veh.substring(0, veh.indexOf("----") - 1);
+            return veh;
+        }
+
         $scope.GetVehicleImage = function(veh) {
             var html = "./assets/images/image_";
             veh = "" + veh;
@@ -64,22 +116,6 @@
             return html;
         }
 
-        function GenerateDestinationFilters(data) {
-            var html = "<label>Destinations</label><hr width'=100%'>" + 
-            "<input id='btnDestAll' class='destination-filter-button' type='button' value='All'></input>";
-            var destinations = [];
-            for(var item of data) {
-                if(destinations.includes(item.destination) == false)
-                {
-                    destinations.push(item.destination);
-                }
-            }
-
-            for(var item of destinations) {
-                html += "<input id='btnDest"+ item + "' class='destination-filter-button' type='button' value='" + item + "'></input>";
-            }
-            document.getElementById("destinationsFilter").innerHTML = html;
-        }
         function AddFilterDestBtnsClickEvent() {
             var buttons = document.getElementsByClassName("destination-filter-button");
             for(var btn of buttons) {
