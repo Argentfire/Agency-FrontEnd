9db62b6cc99b8c295a6e8f9541148a3e7edb058f
Argentfire <Silv3rfire@hotmail.com>
2023-06-01 13:43:21 +0300

    Removed JQuery from index and added a Ticket class component

---
+++ js/Components/ticket.js
@@ -0,0 +1,8 @@
+class Ticket {
+    constructor(from, to, distance, price) {
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+        this.price = price;
+    }
+}
